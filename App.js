3933b89802cf0243f18be1c18cc371e71ae30a48
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-20 14:21:17 -0400

    site

--- App.js
+++ App.js
@@ -1,57 +1,194 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NotFound from "./NotFound/NotFound";
-import { routeList } from "./Routes";
-import config from "Config";
-const logo =
-  "https://orbital-clients.s3.amazonaws.com/_Main/Markab-logo-only.svg";
-import offlineStorage from "./OfflineStorage/OfflineStorage";
-import { Notification } from "@markab.io/react";
-import rootStore from "./Store/rootStore";
-import Portfolio from "./Portfolio/Portfolio";
-class App extends React.Component {
-  state = {};
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {}
-  componentWillReceiveProps(nextProps) {}
-  render() {
-    let { user } = this.props;
-    return (
-      <React.Fragment>
-        <Switch>
-          <Route
-            path="/"
-            render={(props) => {
-              return (
-                <Notification
-                  notificationDomainStore={rootStore.notificationDomainStore}
-                >
-                  <Portfolio
-                    title={<>Markab</>}
-                    config={config}
-                    user={user}
-                    offlineStorage={offlineStorage}
-                    logo={logo}
-                    rootStore={rootStore}
-                    routeList={routeList}
-                    {...props}
-                  />
-                </Notification>
-              );
-            }}
-          />
-          <Route
-            path="*"
-            render={({ location, match, history }) => {
-              return <NotFound />;
-            }}
-          />
-        </Switch>
-      </React.Fragment>
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Grid, Typography, Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import SectionServices from "../OrbitalServicesPage/Sections/SectionServices";
+import SectionWork from "../OrbitalHomePage/Sections/SectionWork";
+import SectionTeam from "../OrbitalAboutPage/Sections/SectionTeam";
+import SectionTestimonials from "../OrbitalHomePage/Sections/SectionTestimonials";
+import OrbitalProductItems from "../OrbitalProductPage/OrbitalProductItems";
+import { Transition } from "react-transition-group";
+import "./OrbitalHomePage.css";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    zIndex: 1,
+    paddingTop: "20vh",
+    textAlign: "center",
+    color: theme.palette.secondary.main,
+    backgroundImage: "url('https://orbital-clients.s3.amazonaws.com/_Main/background.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    paddingBottom: "10vh",
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: "30vh",
+    },
+  },
+  main: {
+    backgroundColor: theme.palette.secondary.main,
+    padding: theme.spacing(4),
+  },
+  sectionTitle: {
+    marginBottom: theme.spacing(2),
+    color: theme.palette.primary.main,
+  },
+  description: {
+    color: theme.palette.primary.main,
+  },
+}));
+
+const DURATION = 80;
+
+const Fade = ({
+  in: inProp,
+  children,
+  transitionStyles,
+  defaultStyle,
+  duration,
+  onState,
+}) => (
+  <Transition in={inProp} timeout={duration}>
+    {(state) => {
+      onState(state);
+      return (
+        <span
+          style={{
+            ...defaultStyle,
+            ...transitionStyles[state],
+          }}
+        >
+          {children}
+        </span>
+      );
+    }}
+  </Transition>
+);
+
+const OrbitalHomePage = ({ onContactUs, ...rest }) => {
+  const classes = useStyles();
+  const [visibleIndex, setVisibleIndex] = useState(0);
+  const [inProp, setInProp] = useState(false);
+  const words = [
+    "Don't spend so much just to get your first prototype out.",
+    "Test your idea without writing code.",
+    "Test your idea without having to pay tens of thousands of dollars.",
+    "Test your idea without having to wait three months.",
+  ];
+  const transitionStyles = {
+    entering: { opacity: 0, display: "none" },
+    entered: { opacity: 1, display: "inline" },
+    exiting: { opacity: 0, display: "none" },
+    exited: { opacity: 0, display: "none" },
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    setTimeout(() => setInProp(true), 100);
+    const timer = setTimeout(
+      () => incrementWord(),
+      DURATION * (words[visibleIndex].split("").length - 1) + 1000
     );
-  }
-  componentWillReceiveProps(nextProps) {}
-}
-export default App;
+
+    return () => clearTimeout(timer);
+  }, [visibleIndex]);
+
+  const incrementWord = () => {
+    setInProp(false);
+    setTimeout(() => setInProp(true), 100);
+    setVisibleIndex(visibleIndex === words.length - 1 ? 0 : visibleIndex + 1);
+  };
+
+  const characters = words[visibleIndex].split("");
+
+  return (
+    <div>
+      <Box className={classes.container}>
+        <Grid container justifyContent="center">
+          <Grid item sm={10} md={10}>
+            <Typography variant="h2" component="h2" sx={{ fontWeight: "bold" }}>
+              Let's build you a full stack app in a few days
+            </Typography>
+            <h3 style={{ minHeight: "50px" }}>
+              <span>
+                {characters.map((char, index) => {
+                  const duration = DURATION * index;
+                  const defaultStyle = {
+                    transition: `opacity ${duration}ms ease-in-out`,
+                    opacity: 0,
+                  };
+                  return (
+                    <Fade
+                      key={index}
+                      onState={(state) => {
+                        index === characters.length - 1 && state === "entered";
+                      }}
+                      defaultStyle={defaultStyle}
+                      duration={duration}
+                      transitionStyles={transitionStyles}
+                      in={inProp}
+                    >
+                      {char}
+                    </Fade>
+                  );
+                })}
+              </span>
+            </h3>
+            <br />
+          </Grid>
+        </Grid>
+      </Box>
+      <div className={classNames(classes.main)}>
+        <div className={classes.container}>
+          <div id="about-us">
+            <SectionTeam />
+          </div>
+          <div id="services">
+            <SectionServices />
+          </div>
+          {/* <div id="demos">
+            <Grid container>
+              <Grid item md={8} sm={8} className={classes.textCenter}>
+                <Typography
+                  variant="h3"
+                  component="h3"
+                  className={classes.sectionTitle}
+                >
+                  Demos
+                </Typography>
+                <h5 className={classes.description}>
+                  Check out some of the demos we have put together.
+                </h5>
+              </Grid>
+            </Grid>
+            <OrbitalProductItems {...rest} />
+          </div> */}
+          <div id="clients">
+            <Grid container>
+              <Grid item md={8} sm={8} className={classes.textCenter}>
+                <Typography
+                  variant="h3"
+                  component="h3"
+                  className={classes.sectionTitle}
+                >
+                  Past Clients
+                </Typography>
+              </Grid>
+            </Grid>
+            <SectionTestimonials />
+          </div>
+          <div id="contact-us">
+            <SectionWork onContactUs={onContactUs} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+OrbitalHomePage.propTypes = {
+  onContactUs: PropTypes.func,
+};
+
+export default OrbitalHomePage;
--- Portfolio/OrbitalHomePage/OrbitalHomePage.js
+++ Portfolio/OrbitalHomePage/OrbitalHomePage.js
@@ -1,40 +1,29 @@
-/*!
-
-=========================================================
-* Material Kit PRO React - v1.7.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-pro-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
-import React from "react";
-// nodejs library to set properties for components
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-// nodejs library that concatenates classes
 import classNames from "classnames";
-// core components
-import GridContainer from "Components/Grid/GridContainer.jsx";
-import GridItem from "Components/Grid/GridItem.jsx";
-import Parallax from "Components/Parallax/Parallax.jsx";
-import landingPageStyle from "Assets/jss/material-kit-pro-react/views/landingPageStyle.jsx";
 import { Link } from "react-router-dom";
-// Sections for this page
-import SectionServices from "../OrbitalServicesPage/Sections/SectionServices.jsx";
+import { Grid, Typography } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+import Parallax from "Components/Parallax/Parallax";
+import SectionServices from "../OrbitalServicesPage/Sections/SectionServices";
 import SectionWork from "../OrbitalHomePage/Sections/SectionWork";
-import SectionTeam from "../OrbitalAboutPage/Sections/SectionTeam.jsx";
+import SectionTeam from "../OrbitalAboutPage/Sections/SectionTeam";
 import SectionTestimonials from "../OrbitalHomePage/Sections/SectionTestimonials";
 import OrbitalProductItems from "../OrbitalProductPage/OrbitalProductItems";
 import { Transition } from "react-transition-group";
-import { withStyles, Grid, Typography } from "@material-ui/core";
 import "./OrbitalHomePage.css";
+
+const useStyles = makeStyles((theme) => ({
+  ...landingPageStyle(theme),
+  container: {
+    zIndex: 1,
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: "20vh",
+    },
+    ...theme.container,
+  },
+}));
+
 const DURATION = 80;
 
 const Fade = ({
@@ -62,151 +51,111 @@ const Fade = ({
   </Transition>
 );
 
-class OrbitalHomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visibleIndex: 0,
-      words: [
-        "Don't spend so much just to get your first prototype out.",
-        "Test your idea without writing code.",
-        "Test your idea without having to pay tens of thousands of dollars.",
-        "Test your idea without having to wait three months."
-      ],
-      transitionStyles: {
-        entering: { opacity: 0, display: "none" },
-        entered: { opacity: 1, display: "inline" },
-        exiting: { opacity: 0, display: "none" },
-        exited: { opacity: 0, display: "none" }
-      },
-      in: false
-    };
-  }
-  componentDidMount() {
+const OrbitalHomePage = ({ onContactUs, ...rest }) => {
+  const classes = useStyles();
+  const [visibleIndex, setVisibleIndex] = useState(0);
+  const [inProp, setInProp] = useState(false);
+  const words = [
+    "Don't spend so much just to get your first prototype out.",
+    "Test your idea without writing code.",
+    "Test your idea without having to pay tens of thousands of dollars.",
+    "Test your idea without having to wait three months."
+  ];
+  const transitionStyles = {
+    entering: { opacity: 0, display: "none" },
+    entered: { opacity: 1, display: "inline" },
+    exiting: { opacity: 0, display: "none" },
+    exited: { opacity: 0, display: "none" }
+  };
+
+  useEffect(() => {
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
-    setTimeout(() => this.setState({ in: true }), 100);
-    setTimeout(
-      () => this.incrementWord(),
-      DURATION *
-        (this.state.words[this.state.visibleIndex].split("").length - 1) +
-        1000
+    setTimeout(() => setInProp(true), 100);
+    const timer = setTimeout(
+      () => incrementWord(),
+      DURATION * (words[visibleIndex].split("").length - 1) + 1000
     );
-  }
-  incrementWord() {
-    this.setState({ in: false });
-    setTimeout(() => this.setState({ in: true }), 100);
-    this.setState({
-      visibleIndex:
-        this.state.visibleIndex === this.state.words.length - 1
-          ? 0
-          : this.state.visibleIndex + 1
-    });
-    // if (this.t) {
-    //   return;
-    // }
-    this.t = setTimeout(() => {
-      this.incrementWord();
-      this.t = null;
-    }, DURATION * (this.state.words[this.state.visibleIndex].split("").length - 1) + 1000);
-  }
-  render() {
-    const { classes, onContactUs, ...rest } = this.props;
-    const characters = this.state.words[this.state.visibleIndex].split("");
-    return (
-      <div>
-        <Parallax
-          image={
-            "https://orbital-clients.s3.amazonaws.com/_Main/background.jpg"
-          }
-          filter="dark"
-        >
-          <div className={classes.container}>
-            <Grid justify="center" container>
-              <Grid item sm={10} md={10}>
-                <Typography
-                  style={{ fontWeight: "bold" }}
-                  variant="h2"
-                  component="h2"
-                >
-                  Let's build you a full stack app in a few days
-                </Typography>
-                <h3 style={{ minHeight: "50px" }}>
-                  <span>
-                    {characters.map((char, index) => {
-                      const duration = DURATION * index;
-                      const defaultStyle = {
-                        transition: `opacity ${duration}ms ease-in-out`,
-                        opacity: 0
-                      };
-                      return (
-                        <Fade
-                          onState={state => {
-                            index === characters.length - 1 &&
-                              state === "entered";
-                          }}
-                          defaultStyle={defaultStyle}
-                          duration={duration}
-                          transitionStyles={this.state.transitionStyles}
-                          in={this.state.in}
-                        >
-                          {char}
-                        </Fade>
-                      );
-                    })}
-                  </span>
-                </h3>
-                {/* <Grid style={{ marginTop: "1em" }} container justify="center">
-                  <p className={"neon-text"}>
-                    <a
-                      href="http://markab-io.s3-website-us-east-1.amazonaws.com/#/"
-                      target="_blank"
-                      className={"neon-link"}
-                    >
-                      See our demos!
-                    </a>
-                  </p>
-                </Grid> */}
-                <br />
-              </Grid>
-            </Grid>
-          </div>
-        </Parallax>
-        <div className={classNames(classes.main, classes.mainRaised)}>
-          <div className={classes.container}>
-            <div id="about-us">
-              <SectionTeam />
-            </div>
-            <div id="services">
-              <SectionServices />
-            </div>
-            {/* <div id="demos">
-              <GridContainer>
-                <GridItem
-                  md={8}
-                  sm={8}
-                  className={classNames(
-                    classes.mlAuto,
-                    classes.mrAuto,
-                    classes.textCenter
-                  )}
-                >
-                  <Typography
-                    variant="h3"
-                    component="h3"
-                    className={classes.sectionTitle}
-                  >
-                    Demos
-                  </Typography>
-                  <h5 className={classes.description}>
-                    Check out some of the demos we have put together.
-                  </h5>
-                </GridItem>
-              </GridContainer>
-              <OrbitalProductItems {...rest} />
-            </div> */}
 
-            {/* <GridContainer>
+    return () => clearTimeout(timer);
+  }, [visibleIndex]);
+
+  const incrementWord = () => {
+    setInProp(false);
+    setTimeout(() => setInProp(true), 100);
+    setVisibleIndex(visibleIndex === words.length - 1 ? 0 : visibleIndex + 1);
+  };
+
+  const characters = words[visibleIndex].split("");
+
+  return (
+    <div>
+      <Parallax
+        image={"https://orbital-clients.s3.amazonaws.com/_Main/background.jpg"}
+        filter="dark"
+      >
+        <div className={classes.container}>
+          <Grid justifyContent="center" container>
+            <Grid item sm={10} md={10}>
+              <Typography
+                style={{ fontWeight: "bold" }}
+                variant="h2"
+                component="h2"
+              >
+                Let's build you a full stack app in a few days
+              </Typography>
+              <h3 style={{ minHeight: "50px" }}>
+                <span>
+                  {characters.map((char, index) => {
+                    const duration = DURATION * index;
+                    const defaultStyle = {
+                      transition: `opacity ${duration}ms ease-in-out`,
+                      opacity: 0
+                    };
+                    return (
+                      <Fade
+                        key={index}
+                        onState={state => {
+                          index === characters.length - 1 &&
+                            state === "entered";
+                        }}
+                        defaultStyle={defaultStyle}
+                        duration={duration}
+                        transitionStyles={transitionStyles}
+                        in={inProp}
+                      >
+                        {char}
+                      </Fade>
+                    );
+                  })}
+                </span>
+              </h3>
+              {/* <Grid style={{ marginTop: "1em" }} container justify="center">
+                <p className={"neon-text"}>
+                  <a
+                    href="http://markab-io.s3-website-us-east-1.amazonaws.com/#/"
+                    target="_blank"
+                    className={"neon-link"}
+                  >
+                    See our demos!
+                  </a>
+                </p>
+              </Grid> */}
+              <br />
+            </Grid>
+          </Grid>
+        </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <div id="about-us">
+            <SectionTeam />
+          </div>
+          <div id="services">
+            <SectionServices />
+          </div>
+          {/* <div id="demos">
+            <GridContainer>
               <GridItem
                 md={8}
                 sm={8}
@@ -216,43 +165,66 @@ class OrbitalHomePage extends React.Component {
                   classes.textCenter
                 )}
               >
-                <Typography variant="h3" component="h3" className={classes.sectionTitle}>Our Clients</Typography>
-              </GridItem>
-            </GridContainer> */}
-            <div id="clients">
-              <GridContainer>
-                <GridItem
-                  md={8}
-                  sm={8}
-                  className={classNames(
-                    classes.mlAuto,
-                    classes.mrAuto,
-                    classes.textCenter
-                  )}
+                <Typography
+                  variant="h3"
+                  component="h3"
+                  className={classes.sectionTitle}
                 >
-                  <Typography
-                    variant="h3"
-                    component="h3"
-                    className={classes.sectionTitle}
-                  >
-                    Past Clients
-                  </Typography>
-                </GridItem>
-              </GridContainer>
-              <SectionTestimonials />
-            </div>
-            <div id="contact-us">
-              <SectionWork onContactUs={onContactUs} />
-            </div>
+                  Demos
+                </Typography>
+                <h5 className={classes.description}>
+                  Check out some of the demos we have put together.
+                </h5>
+              </GridItem>
+            </GridContainer>
+            <OrbitalProductItems {...rest} />
+          </div> */}
+          {/* <GridContainer>
+            <GridItem
+              md={8}
+              sm={8}
+              className={classNames(
+                classes.mlAuto,
+                classes.mrAuto,
+                classes.textCenter
+              )}
+            >
+              <Typography variant="h3" component="h3" className={classes.sectionTitle}>Our Clients</Typography>
+            </GridItem>
+          </GridContainer> */}
+          <div id="clients">
+            <GridContainer>
+              <GridItem
+                md={8}
+                sm={8}
+                className={classNames(
+                  classes.mlAuto,
+                  classes.mrAuto,
+                  classes.textCenter
+                )}
+              >
+                <Typography
+                  variant="h3"
+                  component="h3"
+                  className={classes.sectionTitle}
+                >
+                  Past Clients
+                </Typography>
+              </GridItem>
+            </GridContainer>
+            <SectionTestimonials />
+          </div>
+          <div id="contact-us">
+            <SectionWork onContactUs={onContactUs} />
           </div>
         </div>
       </div>
-    );
-  }
-}
-
-OrbitalHomePage.propTypes = {
-  classes: PropTypes.object
+    </div>
+  );
 };
 
-export default withStyles(landingPageStyle)(OrbitalHomePage);
+OrbitalHomePage.propTypes = {
+  onContactUs: PropTypes.func
+};
+
+export default OrbitalHomePage;
